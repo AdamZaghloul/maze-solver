37fd2af8255abdee7109bdf4854833bcd513762c
AdamZaghloul <adam.zaghloul95@gmail.com>
2024-09-27 08:57:07 -0400

    Added cells with drawing capabilities.

---
+++ cell.py
@@ -0,0 +1,39 @@
+from window import *
+from line import *
+from point import *
+
+class Cell():
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = 0
+        self._x2 = 0
+        self._y1 = 0
+        self._y2 = 0
+        self._win = win
+    
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+
+        if self.has_left_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self._y2)), "black")
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)), "black")
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)), "black")
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)), "black")
+    
+    def draw_move(self, to_cell, undo=False):
+
+        fill = "red"
+        if undo:
+            fill = "gray"
+
+        line = Line(Point((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2), Point((to_cell._x1 + to_cell._x2) / 2, (to_cell._y1 + to_cell._y2) / 2)).draw(self._win.canvas, fill)
+
--- main.py
+++ main.py
@@ -1,14 +1,52 @@
 from window import *
 from line import *
 from point import *
+from cell import *
 
 def main():
     win = Window(800, 600)
 
-    win.draw_line(Line(Point(0,0), Point(600,100)), "black")
-    win.draw_line(Line(Point(300,200), Point(500,700)), "red")
-    win.draw_line(Line(Point(150,400), Point(100,200)), "black")
+    cell1 = Cell(win)
+    cell1.has_right_wall = False
+    cell1.draw(50,50,150,150)
+
+    cell2 = Cell(win)
+    cell2.has_left_wall = False
+    cell2.has_right_wall = False
+    cell2.draw(150,50,250,150)
     
+    cell3 = Cell(win)
+    cell3.has_left_wall = False
+    cell3.has_bottom_wall = False
+    cell3.draw(250,50,350,150)
+
+    cell4 = Cell(win)
+    cell4.has_top_wall = False
+    cell4.draw(50,150,150,250)
+
+    cell5 = Cell(win)
+    cell5.has_top_wall = False
+    cell5.draw(150,150,250,250)
+    
+    cell6 = Cell(win)
+    cell6.has_top_wall = False
+    cell6.draw(250,150,350,250)
+
+    cell7 = Cell(win)
+    cell7.draw(50,250,150,350)
+
+    cell8 = Cell(win)
+    cell8.draw(150,250,250,350)
+    
+    cell9 = Cell(win)
+    cell9.draw(250,250,350,350)
+
+    cell1.draw_move(cell2)
+    cell2.draw_move(cell3)
+    cell3.draw_move(cell6)
+    cell6.draw_move(cell5)
+    cell6.draw_move(cell5, True)
+
     win.wait_for_close()
 
 main()
